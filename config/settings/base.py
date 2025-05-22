c7279bb6ca687d4df5f0038059868aaee1cdc018
Abror <izzatullaev2001abror@gmail.com>
2025-05-22 15:19:20 +0500

    README.md updated

--- config/settings/base.py
+++ config/settings/base.py
@@ -191,6 +191,7 @@ REDIS_URL = env("REDIS_URL", default="redis://localhost:6379/3")
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication",),
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
